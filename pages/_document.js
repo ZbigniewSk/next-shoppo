d9c83c7bce6edd558ed678e74d04bf79e55812f8
zbigniew <zbyszski@gmail.com>
2022-05-28 14:10:12 +0200

    ci: crossOgigin in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,11 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Roboto:wght@300;400;500;700&display=swap"
             rel="stylesheet"
