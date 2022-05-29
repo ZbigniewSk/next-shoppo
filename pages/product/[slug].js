03b635d2f5d8d1ee5e4500956ddb70222b3a0d2d
zbigniew <zbyszski@gmail.com>
2022-05-29 22:21:21 +0200

    build: fetching data from server using get-server-side-props function

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -10,15 +10,16 @@ import {
 } from '@mui/material';
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
 import Layout from '../../components/Layout';
-import data from '../../utils/data';
+import Product from '../../models/Product';
+import db from '../../utils/db';
 
-export default function ProductScreen() {
-  const router = useRouter();
-  const { slug } = router.query;
-  const product = data.products.find((a) => a.slug === slug);
+export default function ProductScreen(props) {
+  const { product } = props;
+  //   const router = useRouter();
+  //   const { slug } = router.query;
+  //   const product = data.products.find((a) => a.slug === slug);
   if (!product) {
     return <div>Product No Found</div>;
   } else {
@@ -106,4 +107,17 @@ export default function ProductScreen() {
       </Layout>
     );
   }
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
 }
