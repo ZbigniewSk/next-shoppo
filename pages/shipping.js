4cb58b801a65395366c16884807d91a0e6d33175
zbigniew <zbyszski@gmail.com>
2022-06-03 20:19:37 +0200

    feat: adding login and shipping page

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,8 +1,7 @@
 import { useRouter } from 'next/router';
-import React from 'react';
 
 export default function Shipping() {
   const router = useRouter();
   router.push('/login');
-  return <div>Shippnig</div>;
+  return;
 }
